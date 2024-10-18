0e33501eba3736b3d55d7a5876a07870a85be930
Rhea Bhattacharjee <rheab@mit.edu>
2024-10-17 21:32:45 -0400

    add post page

---
+++ client/views/PostView.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import PostListComponent from "@/components/Post/PostListComponent.vue";
+</script>
+
+<template>
+  <main>
+    <h1>My Posts</h1>
+    <PostListComponent />
+  </main>
+</template>
+
+<style scoped>
+h1 {
+  text-align: center;
+}
+</style>
